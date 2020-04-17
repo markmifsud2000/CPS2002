141bbdc588744c673b741e02f9a367315d324259
Mark Mifsud <markmfisud2000@gmail.com>
2020-04-18 00:42:38 +0200

    Created unit tests for player movement

---
+++ src/test/java/mark/cps2002/PlayerTest.java
@@ -0,0 +1,60 @@
+package mark.cps2002;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    Player p;
+
+    @Before
+    public void setup() {
+        Position start = new Position(5,5);
+        p = new Player(0,10,10,start);
+    }
+
+    @After
+    public void teardown() {
+        p = null;
+    }
+
+    @Test
+    public void move_up_changes_position() {
+        p.move(Direction.UP);
+
+        //Y value should increase by 1
+        assertEquals(5,p.getPosition().getX());
+        assertEquals(6,p.getPosition().getY());
+    }
+
+    @Test
+    public void move_down_changes_position() {
+        p.move(Direction.DOWN);
+
+        //Y value should decrease by 1
+        assertEquals(5, p.getPosition().getX());
+        assertEquals(4, p.getPosition().getY());
+    }
+
+    @Test
+    public void move_left_changes_position() {
+        p.move(Direction.LEFT);
+
+        //X value should decrease by 1
+        assertEquals(4,p.getPosition().getX());
+        assertEquals(5,p.getPosition().getY());
+    }
+
+    @Test
+    public void move_right_changes_position() {
+        p.move(Direction.RIGHT);
+
+        //X value should increase by 1
+        assertEquals(6,p.getPosition().getX());
+        assertEquals(5,p.getPosition().getY());
+    }
+
+}
