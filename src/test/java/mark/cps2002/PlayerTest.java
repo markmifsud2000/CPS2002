3d712be5af96f525b1d18b47bddc3796cb3bd674
Mark Mifsud <markmfisud2000@gmail.com>
2020-04-18 02:03:16 +0200

    Added unit tests to check if player movement stays within the bounds of the map.

--- src/test/java/mark/cps2002/PlayerTest.java
+++ src/test/java/mark/cps2002/PlayerTest.java
@@ -61,4 +61,68 @@ public class PlayerTest {
         assertEquals(5,p.getPosition().getY());
     }
 
+    @Test
+    public void move_up_outOfMap() {
+        boolean b = true;
+
+        //Move up until the player is out of bounds
+        for (int i = 0; i < 6; i++) {
+            b = p.move(Direction.UP);
+        }
+
+        //Player should be in top row (ie. still in map)
+        assertEquals(false,b);
+        assertEquals(5,p.getPosition().getX());
+        assertEquals(0,p.getPosition().getY());
+
+    }
+
+    @Test
+    public void move_down_outOfMap() {
+        boolean b = true;
+
+        //Move down until the player is out of bounds
+        for (int i = 0; i < 5; i++) {
+            b = p.move(Direction.DOWN);
+        }
+
+        //Player should be in bottom row (ie. still in map)
+        assertEquals(false,b);
+        assertEquals(5,p.getPosition().getX());
+        assertEquals(9,p.getPosition().getY());
+
+    }
+
+    @Test
+    public void move_left_outOfMap() {
+        boolean b = true;
+
+        //Move left until the player is out of bounds
+        for (int i = 0; i < 6; i++) {
+            b = p.move(Direction.LEFT);
+        }
+
+        //Player should be in first column (ie. still in map)
+        assertEquals(false,b);
+        assertEquals(0,p.getPosition().getX());
+        assertEquals(5,p.getPosition().getY());
+
+    }
+
+    @Test
+    public void move_right_outOfMap() {
+        boolean b = true;
+
+        //Move right until the player is out of bounds
+        for (int i = 0; i < 5; i++) {
+            b = p.move(Direction.RIGHT);
+        }
+
+        //Player should be in top row (ie. still in map)
+        assertEquals(false,b);
+        assertEquals(9,p.getPosition().getX());
+        assertEquals(5,p.getPosition().getY());
+
+    }
+
 }
