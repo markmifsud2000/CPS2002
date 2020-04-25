b6625220a61dc5d1ec8f2e5c13c07d270462c49e
Mark Mifsud <markmfisud2000@gmail.com>
2020-04-25 06:39:40 +0200

    Cleared main method

--- src/main/java/mark/cps2002/Launcher.java
+++ src/main/java/mark/cps2002/Launcher.java
@@ -4,35 +4,6 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        Map m = new Map(10, 10);
-        m.generate();
-
-
-        for (int i = 0; i < m.getWidth(); i++) {
-            for (int j = 0; j < m.getHeight(); j++) {
-                Position pos = new Position(i, j);
-                String s;
-
-                switch (m.getTileType(pos)) {
-                    case GRASS:
-                        s = "G";
-                        break;
-                    case WATER:
-                        s = "W";
-                        break;
-                    case TREASURE:
-                        s = "T";
-                        break;
-                    default:
-                        s = "?";
-                        break;
-                }
-
-                System.out.print(s + " ");
-            }
-            System.out.println();
-        }
-
     }
 
 }
