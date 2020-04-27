01d722b99c610206d0c44372f95d08a65151db34
Mark Mifsud <markmfisud2000@gmail.com>
2020-04-27 04:39:40 +0200

    Implemented Getters for Game

--- src/main/java/mark/cps2002/Game.java
+++ src/main/java/mark/cps2002/Game.java
@@ -2,8 +2,21 @@ package mark.cps2002;
 
 public class Game {
 
-    public Game(int noOfPlayers, int boardWidth, int boardHeight) {
+    final int MIN_PLAYERS = 2;              //The minimum number of players
+    final int MAX_PLAYERS = 8;              //The maximum number of players
+    final int SMALL_BOARD_PLAYER_LIMIT = 4; //The number of players for which the minimum board size changes
+    final int MIN_BOARD_SIZE_SMALL = 5;     //The minimum board size for few players
+    final int MIN_BOARD_SIZE_BIG = 8;       //The minimum board size for many players
 
+    Player[] players;       //The players currently in the game
+    Map map;                //The map being played on
+    int turn;               //The current turn number
+    boolean gameFinished;   //Whether or not the game has been finished yet
+
+    public Game(int noOfPlayers, int boardWidth, int boardHeight) {
+        this.players = new Player[noOfPlayers];
+        this.turn = 0;
+        this.gameFinished = false;
     }
 
     public void startGame() {
@@ -15,15 +28,15 @@ public class Game {
     }
 
     public int getNumberOfPlayers() {
-        return 0;
+        return players.length;
     }
 
     public int getTurnNumber() {
-        return 0;
+        return turn;
     }
 
     public boolean isGameFinished() {
-        return false;
+        return gameFinished;
     }
 
 }
